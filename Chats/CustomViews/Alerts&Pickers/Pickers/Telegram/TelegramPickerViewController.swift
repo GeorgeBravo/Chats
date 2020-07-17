23723cbca6624cea05344369c2c13ea07466b8fe
Mykhailo Hetmanskyi <mykhailo.hetmanskyi@chisw.com>
2020-07-17 14:50:25 +0300

    PHAsset+Extensions added

--- Chats/CustomViews/Alerts&Pickers/Pickers/Telegram/TelegramPickerViewController.swift
+++ Chats/CustomViews/Alerts&Pickers/Pickers/Telegram/TelegramPickerViewController.swift
@@ -414,32 +414,3 @@ extension TelegramPickerViewController: UITableViewDataSource {
         return cell
     }
 }
-
-
-extension PHAsset {
-    
-    var image : UIImage {
-        var thumbnail = UIImage()
-        let imageManager = PHCachingImageManager()
-        imageManager.requestImage(for: self, targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil, resultHandler: { image, _ in
-            thumbnail = image!
-        })
-        return thumbnail
-    }
-    
-    var originalFilename: String? {
-        
-        var fname:String?
-        if #available(iOS 9.0, *) {
-            let resources = PHAssetResource.assetResources(for: self)
-            if let resource = resources.first {
-                fname = resource.originalFilename
-            }
-        }
-        if fname == nil {
-            // this is an undocumented workaround that works as of iOS 9.1
-            fname = self.value(forKey: "filename") as? String
-        }
-        return fname
-    }
-}
