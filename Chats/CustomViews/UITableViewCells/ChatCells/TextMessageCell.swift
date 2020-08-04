2c4443f576110890ffda7b4745e64491e29c7479
sava.chormyi <sava.chornyi@chisw.com>
2020-08-04 17:59:13 +0300

    - temp version of textMessaegCell layout

--- Chats/CustomViews/UITableViewCells/ChatCells/TextMessageCell.swift
+++ Chats/CustomViews/UITableViewCells/ChatCells/TextMessageCell.swift
@@ -8,29 +8,20 @@
 
 import UIKit
 
+private struct Constants {
+    static let textViewLineSpacing: CGFloat = 3.0
+    static let textViewFontSize: CGFloat = 16.0
+}
+
 final class TextMessageCell: MessageContentCell, TableViewCellSetup {
     
-    //MARK: Lifecycle
+    //MARK: - Variables
     private var observer: NSKeyValueObservation?
+    private var verticalSpacingConstraint: NSLayoutConstraint?
+    private var heightConstraint: NSLayoutConstraint?
+    private var messageTextViewBottomConstraint: NSLayoutConstraint?
     
-    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupViews()
-        
-        observer = horizontalStackView.layer.observe(\.bounds) { object, _ in
-        }
-    }
-    
-    required public init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public override func prepareForReuse() {
-        super.prepareForReuse()
-        messageTextView.text = ""
-    }
-    
-    // MARK: - Views
+    // MARK: - UI Variables
     private lazy var messageTextView: UITextView = {
         let textView = UITextView()
         
@@ -45,48 +36,62 @@ final class TextMessageCell: MessageContentCell, TableViewCellSetup {
         return textView
     }()
     
-    private var verticalSpacingConstraint: NSLayoutConstraint?
-    private var heightConstraint: NSLayoutConstraint?
+    //MARK: - Inits
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+        
+        observer = horizontalStackView.layer.observe(\.bounds) { object, _ in
+        }
+    }
     
-    // MARK: - Setup
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+    // MARK: - Override logic
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        messageTextView.text = ""
+        messageTextViewBottomConstraint = messageTextView.bottomAnchor.constraint(equalTo: horizontalStackViewContainerView.bottomAnchor, constant: (Constants.textViewFontSize / 2 - 2.0))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutTextViewIfNeeded()
+    }
+
+    // MARK: - Setup logic
     override func setup(with viewModel: TableViewCellModel) {
         super.setup(with: viewModel)
         guard let model = viewModel as? ChatTableViewTextMessageCellModel else { return }
         setupTextView(with: model)
     }
-}
-
-// MARK: - Setup Views
-extension TextMessageCell {
     
     private func setupTextView(with model: ChatTableViewTextMessageCellModel) {
         let style = NSMutableParagraphStyle()
-        style.lineSpacing = 3
+        style.lineSpacing = Constants.textViewLineSpacing
         
         let attributes = [
             NSAttributedString.Key.paragraphStyle: style,
-            .font: UIFont.helveticaNeueFontOfSize(size: 16, style: .regular),
+            .font: UIFont.helveticaNeueFontOfSize(size: Constants.textViewFontSize, style: .regular),
             .foregroundColor: model.isIncomingMessage ? UIColor.black : UIColor.white
         ]
         
         messageTextView.attributedText = NSAttributedString(string: model.message, attributes: attributes)
+        layoutTextViewIfNeeded()
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
-    }
-    
+    // MARK: - Setup Views
     private func setupViews() {
         super.setupSubviews()
-        
         selectionStyle = .none
         
         messageContainerView.addSubview(messageTextView) {
             $0.top == messageContainerView.topAnchor
-            $0.bottom == horizontalStackView.topAnchor
             $0.leading == messageContainerView.leadingAnchor + 5
-            $0.trailing == messageContainerView.trailingAnchor
+            messageTextViewBottomConstraint = $0.bottom == horizontalStackViewContainerView.bottomAnchor + (Constants.textViewFontSize / 2 - 2.0) // Rework for stack height
+            $0.trailing == horizontalStackViewContainerView.trailingAnchor + 5
             
             $0.width >= UIScreen.main.bounds.width * 0.3
             $0.width <= UIScreen.main.bounds.width * 0.6
@@ -94,25 +99,23 @@ extension TextMessageCell {
     }
     
     func layoutTextViewIfNeeded() {
-        heightConstraint?.constant = 100
-        
         let lastGlyphIndex = messageTextView.layoutManager.glyphIndexForCharacter(at: messageTextView.text.count - 1)
-        
         let lastLineFragmentRect = messageTextView.layoutManager.lineFragmentUsedRect(forGlyphAt: lastGlyphIndex, effectiveRange: nil)
+        let lastLineMaxX = lastLineFragmentRect.maxX
         
         print("\n")
-        print(messageTextView.frame)
-        print(horizontalStackView.frame)
-
-        print(lastLineFragmentRect.maxX)
-        print(horizontalStackView.frame.width)
-        print(messageTextView.frame.width - horizontalStackView.frame.width)
+        print("text: \(messageTextView.text)")
+        print("lastLineMaxX = \(lastLineMaxX)")
+        print("messageTextView.frame = \(messageTextView.frame)")
+        print("horizontalStackViewContainerView.frame = \(horizontalStackViewContainerView.frame)")
         
-        if lastLineFragmentRect.maxX > messageTextView.frame.width - horizontalStackView.frame.width {
-
-            verticalSpacingConstraint?.constant = 10
+        guard messageTextView.frame.width > 0 else { return }
+        if horizontalStackViewContainerView.frame.minX < lastLineMaxX {
+            messageTextViewBottomConstraint?.constant = -20
+            print("messageTextViewBottomConstraint?.constant = -20")
         } else {
-            verticalSpacingConstraint?.constant = -10
+            messageTextViewBottomConstraint = messageTextView.bottomAnchor.constraint(equalTo: horizontalStackViewContainerView.bottomAnchor, constant: (Constants.textViewFontSize / 2 - 2.0))
         }
     }
+    
 }
